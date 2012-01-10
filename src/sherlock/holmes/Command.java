224b44a2ba69a33558129f4a4a46a0c69d5b8b9f
Rico <Rico@Rico-PC>
2012-01-10 14:22:44 +0100

    Ik heb een brute verandering gedaan

--- src/sherlock/holmes/Command.java
+++ src/sherlock/holmes/Command.java
@@ -2,6 +2,7 @@
  * This class is part of the "World of Zuul" application. 
  * "World of Zuul" is a very simple, text based adventure game.  
  *
+ * 
  * This class holds information about a command that was issued by the user.
  * A command currently consists of two strings: a command word and a second
  * word (for example, if the command was "take map", then the two strings
