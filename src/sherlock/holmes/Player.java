79843b8a33cd9dfb217c3af255d77cbb0a17de93
Marcellino <Marcellino@192.168.0.104>
2012-01-12 11:19:31 +0100

    Added some extra methods for the player

--- src/sherlock/holmes/Player.java
+++ src/sherlock/holmes/Player.java
@@ -11,10 +11,28 @@ package sherlock.holmes;
 public class Player {
     
     private String playerName;
+    private Item items;
     
     public void Player(String playerName)
     {
         this.playerName = playerName;
+    }
+    
+    /**
+     * Set player name
+     */
+    public void setPlayerName()
+    {
+        this.playerName = playerName;
+    }
+    
+    /**
+     * Return player name
+     * @return String
+     */
+    public String getPlayerName()
+    {
+        return playerName;
     }
     
 }
