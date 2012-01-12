fab1a1094da461d19f76fbcb2730b2b7301f25cc
Marcellino <Marcellino@192.168.0.104>
2012-01-12 10:55:43 +0100

    Create a player class

---
+++ src/sherlock/holmes/Player.java
@@ -0,0 +1,20 @@
+/**
+ *  This class is the player class. This class is used to set the player
+ * his name. The name is used to congratulate the user with it's completed
+ * quest. It is also added to add feature functions.
+ * 
+ * @author  Rico de Feijter, Marcellino van Hecke
+ * @version 1.0
+ */
+package sherlock.holmes;
+
+public class Player {
+    
+    private String playerName;
+    
+    public void Player(String playerName)
+    {
+        this.playerName = playerName;
+    }
+    
+}
