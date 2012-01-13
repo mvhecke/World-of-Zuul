6bf06dc4741781e283c76bbe70f805185219d5ee
Marcellino <Marcellino@192.168.0.104>
2012-01-13 14:58:45 +0100

    Added conversation class

---
+++ src/sherlock/holmes/Conversation.java
@@ -0,0 +1,13 @@
+/**
+ * Class Conversation - a conversation in an adventure game.
+ *
+ * This class is part of the "Sherlock Holmes" application. 
+ * 
+ * @author  Rico de Feijter, Marcellino van Hecke
+ * @version 1.0
+ */
+package sherlock.holmes;
+
+public class Conversation {
+    
+}
