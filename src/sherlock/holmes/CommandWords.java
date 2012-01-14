2a84fe04d3557ef96b616f44b6ffb263924efbf9
Marcellino van Hecke <marcellino@mvanhecke.nl>
2012-01-14 16:03:22 +0100

    Added command word "pak".

--- src/sherlock/holmes/CommandWords.java
+++ src/sherlock/holmes/CommandWords.java
@@ -14,7 +14,7 @@ public class CommandWords
 {
     // a constant array that holds all valid command words
     private static final String[] validCommands = {
-        "ga", "kijk", "stop", "help"
+        "ga", "kijk", "pak", "stop", "help"
     };
 
     /**
