6fe1f30f61e5f648366393d6a0e51466a9ccb2e3
Marcellino van Hecke <marcellino@mvanhecke.nl>
2012-01-14 21:18:13 +0100

    Added command word "tas".

--- src/sherlock/holmes/CommandWords.java
+++ src/sherlock/holmes/CommandWords.java
@@ -14,7 +14,7 @@ public class CommandWords
 {
     // a constant array that holds all valid command words
     private static final String[] validCommands = {
-        "ga", "kijk", "pak", "stop", "help"
+        "ga", "kijk", "pak", "tas", "stop", "help"
     };
 
     /**
